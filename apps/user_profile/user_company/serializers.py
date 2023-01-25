db582fea91591d828b3da1afc81701dd8f4f3f79
Dmitri Kirichko <kiri4ko@gmail.com>
2023-01-25 13:46:28 +0300

    linter

--- apps/user_profile/user_company/serializers.py
+++ apps/user_profile/user_company/serializers.py
@@ -9,6 +9,6 @@ class HeadCompanySerializer(serializers.ModelSerializer):
         fields = [
             'company_name', 'logo', 'website', 'tagline', 'industry', 'software_stack', 'select_industries',
             'short_description', 'full_description', 'average_hourly_rate', 'currency_rate', 'minimum_project_budget',
-            'currency_budget', 'team_size', 'location', 'foundation_date', 'clients_focus', 'contact_marketing', 'contact_expert',
-            'links_case', 'client_describe', 'employees'
+            'currency_budget', 'team_size', 'location', 'foundation_date', 'clients_focus', 'contact_marketing',
+            'contact_expert', 'links_case', 'client_describe', 'employees'
         ]
